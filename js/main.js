ed91dad307c1aca4890f19c2784cdb5c71333f0d
anhonestmouse <anhonestmouse@gmail.com>
2017-11-21 16:53:34 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king","king"];
+var cards = [
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+}
+
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if(cardsInPlay.length === 2){
@@ -9,11 +31,11 @@ var checkForMatch = function(){
 			alert("Sorry, try again.");
 		}
 	}
-}
+};
 var flipCard = function(cardID){
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID])
+	cardsInPlay.push(cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].rank + " of " + cards[cardID].suit + ".  Image: "+cards[cardID].cardImage);
 	checkForMatch();
-}
+};
 flipCard(0);
 flipCard(2);
